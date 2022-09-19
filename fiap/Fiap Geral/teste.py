835aa0566afcc5da16661d229b4aa72821e9101e
Nathalia dos Santos <natalyecabral@gmail.com>
2022-09-19 19:18:56 -0300

    test

---
+++ fiap/Fiap Geral/teste.py
@@ -0,0 +1,8 @@
+valores = [1,7,7,19,3,2,11,15,6,1,5]
+print(f"A lista foi criada assim {valores}")
+contagem = valores.count(7)
+print(f"Nessa lista o número 7 apararece {contagem} vezes")
+valores.reverse()
+print(f"A lista agora está invetida{valores}")
+valores.sort()
+print(f"A lista agora está ordenada {valores}")
